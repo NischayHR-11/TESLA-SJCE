86f213422491d7d4fce7f98a390e2742d51e8f84
NischayHR-11 <nischayhr11@gmail.com>
2024-12-08 11:42:15 +0530

    Implemented Navbar Component

--- Frontend/src/Navbar.jsx
+++ Frontend/src/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
         <>
         <nav class="navbar navbar-expand-lg bg-body-light sticky-top custom" style={{height:"10vh"}}>
             <div class="container-fluid">
-                <a class="navbar-brand nav-item" href="#" style={{display:"flex",justifyContent:"center",alignItems:"center"}}><img src="/Logo.png" alt="" style={{height:"7vh"}}/><h3 className='ms-3'> TESLA</h3></a>
+                <a class="navbar-brand nav-item" href="#" style={{display:"flex",justifyContent:"center",alignItems:"center"}}><img src="/Logo.png" alt="" style={{height:"7vh"}}/><h3 className='ms-3'> TESLA - SJCE </h3></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
@@ -24,7 +24,7 @@ function Navbar() {
                     <li class="nav-item a1" style={{marginRight:"5rem"}}>
                     <a class="nav-link active" href="#">Team</a>
                     </li>
-                    <li class="nav-item a1" style={{marginRight:"2.5rem"}}>
+                    <li class="nav-item a1 sa" style={{marginRight:"2.5rem"}}>
                     <a class="nav-link active" href="#">Developers</a>
                     </li>
                 </ul>
